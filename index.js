d1a425f96f7e341775674aa2bccb2edeab6bcc85
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-06-07 16:01:52 +0500

    4-oy 7-dars

---
+++ index.js
@@ -0,0 +1,128 @@
+//1-misol
+//let a = +prompt("sonni kiriting");
+//switch (a) {
+//  case 1:
+//    console.log("yanvar");
+//    break;
+//  case 2:
+//    console.log("fevral");
+//    break;
+//  case 3:
+//    console.log("mart");
+//    break;
+//  case 4:
+//    console.log("aprel");
+//    break;
+//  case 5:
+//    console.log("may");
+//    break;
+//  case 6:
+//    console.log("iyun");
+//    break;
+//  case 7:
+//    console.log("iyul");
+//    break;
+//  case 8:
+//    console.log("avgust");
+//    break;
+//  case 9:
+//    console.log("sentabr");
+//    break;
+//  case 10:
+//    console.log("oktabr");
+//    break;
+//  case 11:
+//    console.log("noyabr");
+//    break;
+//  case 12:
+//    console.log("dekabr");
+//    break;
+//
+//  default:console.log("bunday oy yo'q");
+//    break;
+//}
+//
+//2-misol
+//let b = +prompt("sonni kiriting");
+//switch (b) {
+//  case 0:
+//    console.log("f");
+//    break;
+//  case 60:
+//    console.log("f");
+//    break;
+//  case 60:
+//    console.log("d");
+//    break;
+//  case 70:
+//    console.log("d");
+//    break;
+//  case 70:
+//    console.log("c");
+//    break;
+//  case 80:
+//    console.log("c");
+//    break;
+//  case 80:
+//    console.log("b");
+//    break;
+//  case 90:
+//    console.log("b");
+//    break;
+//  case 90:
+//    console.log("a");
+//    break;
+//  case 100:
+//    console.log("a");
+//    break;
+//  default:console.log("siz kiritgan bal yo'q");
+//    break;
+//}
+//3-misol
+//let c = +prompt("sonni kiriting");
+//switch (c) {
+//  case 5:
+//    console.log("xayrli kun");
+//    break;
+//  case 5:
+//    console.log("xayrli tong");
+//    break;
+//  case 12:
+//    console.log("xayrli tong");
+//    break;
+//  case 12:
+//    console.log("xayrli kun");
+//    break;
+//  case 17:
+//    console.log("xayrli kun");
+//    break;
+//  case 17:
+//    console.log("xayrli kech");
+//    break;
+//  case 21:
+//    console.log("xayrli kech");
+//    break;
+//  case 21:
+//    console.log("xayrli tun");
+//    break;
+//  case 24:
+//    console.log("xayrli tun");
+//    break;
+//  case 5:
+//    console.log("xayrli tun");
+//    break;
+//  case 0:
+//    console.log("xayrli tun");
+//    break;
+//  default:console.log("bunaqa soat yo'q");
+//    break;
+//}
+// 4-misol
+//let j=0
+//let sum=0
+//while (j<=20) {
+//    if(j%2==0){
+//        sum=sum+j
+//    }
+//}
+//console.log(sum);
